e70eeaca11c3b6b39ad5fe1cd9b1929ee87659f1
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2023-01-21 19:52:01 +0100

    add paquete ivan y clase Ivan.java con los metodos main y miNombre

---
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/ivan/Ivan.java
@@ -0,0 +1,19 @@
+package ivan;
+
+public class Ivan {
+	
+	private static String miNombre() {
+		
+		String miNombre = "Iván";
+	
+		return miNombre;
+	
+	}//END MINOMBRE
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola, soy "+miNombre());
+
+	}//END MAIN
+
+}//END IVAN
