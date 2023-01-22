3f65ca88f300e40c72fc4f5127028c8efde2925a
laura.beltran <laurabeltranp15@gmail.com>
2023-01-22 16:23:43 +0100

    Creando Laura paquete

---
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/Laura/Laura.java
@@ -0,0 +1,16 @@
+package Laura;
+
+public class Laura {
+
+	public static String nombreLaura() {
+		String nombre = "Laura";
+		return nombre;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola soy " + nombreLaura());
+	}
+
+}
