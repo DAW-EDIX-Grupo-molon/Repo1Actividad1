b699461abe791ddebd98a32f75e350b3a9c8ce02
Elogin <86604232+elogin91@users.noreply.github.com>
2023-01-22 17:18:13 +0100

    Creando paquete Cristina

---
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/cristina/Cristina.java
@@ -0,0 +1,11 @@
+package cristina;
+
+public class Cristina {
+	public static void main(String[] args) {
+		printName();
+	}
+
+	private static void printName() {
+		System.out.println("Hola, soy Cristina.");
+	}
+}
