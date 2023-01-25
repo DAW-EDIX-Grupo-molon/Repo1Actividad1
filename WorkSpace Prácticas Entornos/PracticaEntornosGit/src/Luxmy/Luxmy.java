be7da5e4c12fa357c2c58d093ff27994c381bffe
nlarunasalam <nlarunasalam@gmail.com>
2023-01-25 08:57:06 +0100

    paquete

---
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/Luxmy/Luxmy.java
@@ -0,0 +1,17 @@
+package Luxmy;
+
+public class Luxmy {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola, soy " + nombre());
+		
+	}
+	
+	private static String nombre() {
+
+	String nombre = "Luxmy";
+	return nombre;
+	
+	}
+}
