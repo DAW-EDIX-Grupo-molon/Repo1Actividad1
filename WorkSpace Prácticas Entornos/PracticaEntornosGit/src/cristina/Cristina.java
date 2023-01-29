c59409ff4997c1c588de09412f2d0d0b269f3b20
Elogin <86604232+elogin91@users.noreply.github.com>
2023-01-29 18:09:10 +0100

    adding hobbies

--- WorkSpace Prácticas Entornos/PracticaEntornosGit/src/cristina/Cristina.java
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/cristina/Cristina.java
@@ -3,9 +3,14 @@ package cristina;
 public class Cristina {
 	public static void main(String[] args) {
 		printName();
+		printHobbies();
 	}
 
 	private static void printName() {
 		System.out.println("Hola, soy Cristina.");
+	}
+	
+	private static void printHobbies() {
+		System.out.println("Mis tres principales aficiones son jugar a juegos on-line, ir de compras y querer mucho a mi perro Toby.");
 	}
 }
