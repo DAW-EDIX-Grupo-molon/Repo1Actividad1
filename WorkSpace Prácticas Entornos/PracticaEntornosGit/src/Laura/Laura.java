4853a229304798ec490c3ebebe34019720c1f5f4
Elogin <86604232+elogin91@users.noreply.github.com>
2023-01-31 15:48:44 +0100

    adding comidas cris en paquete laura

--- WorkSpace Prácticas Entornos/PracticaEntornosGit/src/Laura/Laura.java
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/Laura/Laura.java
@@ -10,11 +10,16 @@ public class Laura {
 		System.out.println("Y mis hobbies son los shooter, ser entrenadora de baloncesto y árbitro");
 	}
 	
+	public static void misComidas() {
+		System.out.println(" Mis comidas favoritas son macarrones, jamón serrano y las hamburguesas.");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hola soy " + nombreLaura());
 		misHobbies();
+		misComidas();
 	}
 
 }
