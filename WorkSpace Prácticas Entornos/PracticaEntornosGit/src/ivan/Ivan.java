fdbf09cef4eea77ad92f38615d82b582323baa40
nlarunasalam <nlarunasalam@gmail.com>
2023-02-03 22:04:46 +0100

    comidas finales

--- WorkSpace Prácticas Entornos/PracticaEntornosGit/src/ivan/Ivan.java
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/ivan/Ivan.java
@@ -11,11 +11,17 @@ public class Ivan {
 		String hobbies = "deporte, música y videojuegos";
 		return hobbies;
 	}//END hobbies
+	
+	private static String comidas() {
+		String comidas = "tikka masala, sushi y tacos";
+		return comidas;
+	}// Final de comidas, modificado
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, soy "+miNombre());
 		System.out.println("y tres de mis hobbies son: " + hobbies());
+		System.out.println("Mis comidas preferidas son: " + comidas());
 
 	}//END MAIN
 
