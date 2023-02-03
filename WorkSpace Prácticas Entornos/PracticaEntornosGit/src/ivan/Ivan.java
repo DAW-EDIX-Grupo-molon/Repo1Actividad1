b0445f658bbaa9b4f1fb3d6f91225e0c17d5e10e
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2023-02-03 22:00:24 +0100

    definitivo comidas

--- WorkSpace Prácticas Entornos/PracticaEntornosGit/src/ivan/Ivan.java
+++ WorkSpace Prácticas Entornos/PracticaEntornosGit/src/ivan/Ivan.java
@@ -11,11 +11,17 @@ public class Ivan {
 		String hobbies = "deporte, música y videojuegos";
 		return hobbies;
 	}//END hobbies
+	
+	private static String comidas() {
+		String comidas = "Paella, arroz al horno y coca de tomate";
+		return comidas;
+	}//END misComidas
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, soy "+miNombre());
 		System.out.println("y tres de mis hobbies son: " + hobbies());
+		System.out.println("Mis comidas preferidas son: "+comidas());
 
 	}//END MAIN
 
